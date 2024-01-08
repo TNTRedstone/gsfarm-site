e321827030f588aee4e9fa8cf26b0693c58e5690
TNTRedstone <cgsteck@outlook.com>
2024-01-08 23:06:38 +0000

    Build that commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './src/**/*.svelte', // Include your Svelte components
+        './src/**/*.svelte' // Include your Svelte components
     ],
     theme: {
         extend: {}
