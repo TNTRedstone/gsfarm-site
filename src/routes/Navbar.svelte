d8c98d09c4b01ab40897d5f51acc479ef97d56cc
= <=>
2024-01-11 17:05:22 -0500

    Added navbar

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,39 @@
+<nav class="bg-sky-700 p-2 mt-0 w-full">
+    <div class="container mx-auto flex flex-wrap items-center">
+        <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
+            <a class="text-white no-underline hover:text-white hover:no-underline" href="/">
+                <span class="text-2xl pl-2">GSFarm</span>
+            </a>
+        </div>
+        <div class="flex w-full pt-2 content-center justify-center md:w-1/2 md:justify-end">
+            <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
+                <li>
+                    <a
+                        class="rounded bg-sky-400 p-2 m-1 text-center hover:bg-sky-500 nav-buttons"
+                        href="/countdowns">Countdowns</a
+                    >
+                </li>
+                <li>
+                    <a
+                        class="rounded bg-sky-400 p-2 m-1 text-center hover:bg-sky-500 nav-buttons"
+                        href="/animal-albums">Animal Albums</a
+                    >
+                </li>
+                <li>
+                    <a
+                        class="rounded bg-sky-400 p-2 m-1 text-center hover:bg-sky-500 nav-buttons"
+                        href="/blog">Blog</a
+                    >
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<style>
+    @media screen and (max-width: 640px) {
+        .nav-buttons {
+            display: none;
+        }
+    }
+</style>
--- src/routes/countdowns/countdown.svelte
+++ src/routes/countdowns/countdown.svelte
@@ -17,7 +17,12 @@
             let minutes = Math.floor((differenceInMilliseconds % (1000 * 60 * 60)) / (1000 * 60));
             let seconds = Math.floor((differenceInMilliseconds % (1000 * 60)) / 1000);
 
-            countdown = [days, hours, minutes, seconds];
+            countdown = [
+                days.toString() + ' days',
+                hours.toString() + ' hours',
+                minutes.toString() + ' minutes',
+                seconds.toString() + ' seconds'
+            ];
         } else {
             countdown = ['Event has passed'];
         }
@@ -28,9 +33,8 @@
 </script>
 
 <div class="bg-sky-300 rounded">
-    <h2 class="sm:text-sm md:text-md lg:text-lg xl:text-xl">{label}</h2>
-    <li class="sm:text-sm md:text-md lg:text-lg">{countdown[0]} days</li>
-    <li class="sm:text-sm md:text-md lg:text-lg">{countdown[1]} hours</li>
-    <li class="sm:text-sm md:text-md lg:text-lg">{countdown[2]} minutes</li>
-    <li class="sm:text-sm md:text-md lg:text-lg">{countdown[3]} seconds</li>
+    <h2 class="sm:text-sm md:text-md lg:text-lg xl:text-xl">{label}:</h2>
+    {#each countdown as event}
+        <li class="sm:text-sm md:text-md lg:text-lg">{event}</li>
+    {/each}
 </div>
